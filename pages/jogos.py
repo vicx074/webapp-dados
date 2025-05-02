499a8d3bcfdc4c80e35a37f0869664a035c1ddc4
Victor Eduardo <victororg22@gmail.com>
2025-05-02 15:35:48 -0300

    adicionar funcionalidade para exibir jogos do dia com data de análise

--- pages/jogos.py
+++ pages/jogos.py
@@ -4,5 +4,18 @@ import numpy as np
 import datetime
 from datetime import date
 
-st.title("Página de Jogos")
-st.write("Conteúdo da página de jogos.")
+st.title("Jogos do dia")
+
+dia = st.date_input(
+    "Data de análise",
+    date.today()
+)
+
+def load_data_jogos():
+    url = f"https://github.com/futpythontrader/YouTube/blob/main/Jogos_do_Dia/FootyStats/Jogos_do_Dia_FootyStats_{dia}.csv?raw=true"
+    data_jogos = pd.read_csv(url)
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
